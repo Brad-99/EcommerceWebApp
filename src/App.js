5e014526d60ba4dbb51b7fc6cbd6c1897447690d
Brad <rayray21211@gmail.com>
2023-09-11 17:12:30 +0800

    header.js changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
+    // BEM
     <div className="App">
       <h1>Hello ooooo</h1>
-
+      <Header />
     </div>
   );
 }
