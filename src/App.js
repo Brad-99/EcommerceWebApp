fa59f67931845e109cd5b63cb49837cadb29af8c
Brad <rayray21211@gmail.com>
2023-11-20 13:01:02 +0800

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route }
+from "react-router-dom";
 
 function App() {
   return (
