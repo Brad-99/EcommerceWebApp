8268d02b78bfb27e6672d42622c2ca3f197aa592
BradpuppyC <rayray21211@gmail.com>
2023-12-23 23:35:25 +0800

    2.55.58 add to basket done

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './CheckoutProduct.css'
+
+function CheckoutProduct({ id, image, title, price, rating}) {
+  return (
+    <div clasName = 'checkoutProduct'>
+        <img className='checkoutProduct__image' src={image} />
+  
+        <div className='checkoutProduct__info'>
+            <p className='checkoutProduct__title'>{title}</p>
+            <p className='checkoutProduct__price'></p>
+            <small>$</small>
+            <strong>{price}</strong>
+            <div className="checkoutProduct__rating">
+                {Array(rating)
+                .fill()
+                .map((_, i) => (
+                    <p>🌟</p>
+                ))}
+            </div>
+            <button>Remove from Basket</button>
+        </div>
+    </div>
+  )
+}
+
+export default CheckoutProduct
