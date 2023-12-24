307e093c6ed55cfb8b292ca74c30757be168a375
BradpuppyC <rayray21211@gmail.com>
2023-12-24 18:33:45 +0800

    3.4652

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import { Link, useHistory } from "react-router-dom";
 
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault()
+
+        // some fancy firebase login shitttt...
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        // do some fancy firebase register shit...
+    }
+
   return (
     <div className='login'>
         <Link to='/'>
@@ -18,11 +33,26 @@ function Login() {
 
             <form>
                 <h5>E-mail</h5>
-                <input type='text'/>
+                <input type='text' value={email} onChange=
+                {e => setEmail(e.target.value)}/>
 
                 <h5>Password</h5>
-                <input type='text' />
+                <input type='password' value={password}
+                onChange={e => setPassword(e.target.value)} />
+
+                <button type='submit' onClick={signIn}
+                className='login__signInButton'>Sign In</button>
             </form>
+
+            <p>
+                By signing-in you agree to Amazon's
+                Conditions of Use & Sale. Please
+                see our Privacy Notice, our Cookies Notice
+                and our Interest-Based Ads Notice.
+            </p>
+
+            <button onClick={register}
+            className='login__registerButton'>Create your Amazon account</button>
         </div>
     </div>
   )
